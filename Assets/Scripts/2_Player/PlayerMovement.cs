bf3e8d4390704d4f098b01fa4d7c2f4d22a45385
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-06-04 15:37:56 +0900

    feat: Player 움직임
    - 좌우
    - 점프

---
+++ Assets/Scripts/2_Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PlayerAbility))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private PlayerAbility ability;
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        ability = GetComponent<PlayerAbility>();
+    }
+
+    public void Move(Vector2 input)
+    {
+        moveDirection = new Vector3(input.x * ability.MoveSpeed, rb.velocity.y, 0);
+        rb.velocity = moveDirection;
+
+        if (input.x != 0)
+        {
+            Vector3 newForward = new Vector3(input.x, 0, 0);
+            transform.forward = newForward;
+        }
+    }
+}
