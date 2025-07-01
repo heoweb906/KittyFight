1ec4c6ac579491f83204f305f28d6baf737fb42c
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-07-01 14:00:27 +0900

    feat: Dash 할당

--- Assets/Scripts/2_Player/PlayerController.cs
+++ Assets/Scripts/2_Player/PlayerController.cs
@@ -3,21 +3,25 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMovement))]
 [RequireComponent(typeof(PlayerJump))]
 [RequireComponent(typeof(PlayerAttack))]
+[RequireComponent(typeof(PlayerDash))]
 public class PlayerController : MonoBehaviour
 {
     private PlayerMovement movement;
     private PlayerJump jump;
     private PlayerAttack attack;
+    private PlayerDash dash;
 
     private Vector2 moveInput;
     private bool jumpInput;
     private bool attackInput;
+    private bool dashInput;
 
     private void Awake()
     {
         movement = GetComponent<PlayerMovement>();
         jump = GetComponent<PlayerJump>();
         attack = GetComponent<PlayerAttack>();
+        dash = GetComponent<PlayerDash>();
     }
 
     private void Update()
@@ -26,6 +30,7 @@ public class PlayerController : MonoBehaviour
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         jumpInput = Input.GetKeyDown(KeyCode.Space);
         attackInput = Input.GetMouseButtonDown(0);
+        dashInput = Input.GetMouseButtonDown(1);
 
         // 점프 입력 처리
         if (jumpInput)
@@ -37,6 +42,11 @@ public class PlayerController : MonoBehaviour
         if (attackInput)
         {
             attack.TryAttack();
+        }
+
+        if (dashInput)
+        {
+            dash.TryDash();
         }
     }
 
