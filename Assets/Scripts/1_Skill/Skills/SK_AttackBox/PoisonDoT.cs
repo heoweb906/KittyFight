f816a70e8fe065d75efb0896f5e78e5061d93768
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-08-05 13:57:02 +0900

    feat: FangShot Skill
    - Poison 중독 상태 추가

---
+++ Assets/Scripts/1_Skill/Skills/SK_AttackBox/PoisonDoT.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class PoisonDoT : MonoBehaviour
+{
+    private Coroutine poisonRoutine;
+
+    public void ApplyPoison(float duration, float interval, int damage)
+    {
+        if (poisonRoutine != null)
+        {
+            StopCoroutine(poisonRoutine);
+        }
+
+        poisonRoutine = StartCoroutine(DoPoison(duration, interval, damage));
+    }
+
+    private IEnumerator DoPoison(float duration, float interval, int damage)
+    {
+        float elapsed = 0f;
+        PlayerHealth health = GetComponent<PlayerHealth>();
+
+        while (elapsed < duration)
+        {
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+            }
+
+            yield return new WaitForSeconds(interval);
+            elapsed += interval;
+        }
+
+        Destroy(this);
+    }
+}
