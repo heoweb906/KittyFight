5612bb0632062f1005316dd63f05c3d1f7bca205
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-08-14 19:10:05 +0900

    feat: Skill Type 추가
    - Dash 도 스킬로 취급

--- Assets/Scripts/1_Skill/Skills/SkillType.cs
+++ Assets/Scripts/1_Skill/Skills/SkillType.cs
@@ -2,6 +2,7 @@ public enum SkillType
 {
     Melee = 1,    // UI ½½·Ô 1
     Ranged = 2,   // UI ½½·Ô 2
-    Skill1 = 3,   // UI ½½·Ô 3
-    Skill2 = 4    // UI ½½·Ô 4
+    Dash = 3,     // UI ½½·Ô 3
+    Skill1 = 4,   // UI ½½·Ô 4
+    Skill2 = 5    // UI ½½·Ô 5
 }
