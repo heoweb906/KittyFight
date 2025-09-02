901f29cfe68c1b54c6eb75189c042d775d0b73a7
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-09-02 12:29:56 +0900

    feat: Finisher 구현

---
+++ Assets/Scripts/1_Skill/Passive/PS_Finisher.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PS_Finisher : Passive
+{
+    [Range(0f, 1f)] public float threshold = 0.10f;
+
+    protected override void Subscribe(AbilityEvents e)
+        => e.OnBeforeDealDamage += OnBeforeDealDamage;
+    protected override void Unsubscribe(AbilityEvents e)
+        => e.OnBeforeDealDamage -= OnBeforeDealDamage;
+
+    void OnBeforeDealDamage(ref int dmg, GameObject victimGO)
+    {
+        var hp = victimGO.GetComponentInParent<PlayerHealth>();
+        if (!hp) return;
+
+        int cur = hp.CurrentHP;
+        int max = hp.MaxHP;
+
+        if (max <= 0 || cur <= 0) return;
+
+        if (cur <= Mathf.CeilToInt(max * threshold))
+        {
+            dmg = cur; // 이번 히트로 즉사
+        }
+    }
+}
