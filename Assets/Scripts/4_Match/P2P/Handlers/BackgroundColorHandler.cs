c13456aeed6fc32ff20e97960a0c7e11296a8e9f
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-09-30 15:42:18 +0900

    refactor: Map 관련 P2P Message 수정
    - Map Index 로 변경
    - BackGround도 Index 로 변경

--- Assets/Scripts/4_Match/P2P/Handlers/BackgroundColorHandler.cs
+++ Assets/Scripts/4_Match/P2P/Handlers/BackgroundColorHandler.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class BackgroundColorHandler : IP2PMessageHandler
 {
+    private readonly MapManager mapManager;
     private readonly GameManager gameManager;
 
     public BackgroundColorHandler(GameManager gm)
@@ -15,16 +16,7 @@ public class BackgroundColorHandler : IP2PMessageHandler
     {
         string json = msg.Substring(7);
         var data = JsonUtility.FromJson<BackgroundColorMessage>(json);
-        Color color = new Color(data.r, data.g, data.b);
 
-        gameManager.ApplyBackgroundColor(color);
-
-
-
-        if (data.iMapGimicNum >= 1)
-        {
-            gameManager.IntMapGimicnumber = data.iMapGimicNum;
-            gameManager.BoolAcitveMapGimic = true;
-        }
+        gameManager.OnReceiveSetupMessage(data);
     }
 }
