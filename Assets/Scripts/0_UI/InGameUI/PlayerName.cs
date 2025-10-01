84382439388f4e955841cc067bae7c0cdd2a4394
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-10-02 01:54:47 +0900

    feat: Player NickName
    - Player랑 상관없이 안 뒤집히도록

---
+++ Assets/Scripts/0_UI/InGameUI/PlayerName.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerName : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+}
