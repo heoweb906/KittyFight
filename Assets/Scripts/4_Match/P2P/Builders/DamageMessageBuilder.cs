e4e14cf18669d3a018f2450ae6b3ef9b48fd2f2a
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-10-18 21:36:28 +0900

    feat: 데미지 맞는 방향 추가

--- Assets/Scripts/4_Match/P2P/Builders/DamageMessageBuilder.cs
+++ Assets/Scripts/4_Match/P2P/Builders/DamageMessageBuilder.cs
@@ -4,7 +4,18 @@ public static class DamageMessageBuilder
 {
     public static string Build(int targetPlayer, int hp)
     {
-        var msg = new DamageMessage { targetPlayer = targetPlayer, hp = hp };
+        return Build(targetPlayer, hp, null);
+    }
+
+    public static string Build(int targetPlayer, int hp, Vector3? sourceWorldPos)
+    {
+        var msg = new DamageMessage
+        {
+            targetPlayer = targetPlayer,
+            hp = hp,
+            hasSource = sourceWorldPos.HasValue,
+            sourceWorldPos = sourceWorldPos ?? Vector3.zero
+        };
         return "[DMG]" + JsonUtility.ToJson(msg);
     }
 }
