b796d15f27041f29dff98cee741d64bb17e6831c
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-11-26 18:17:43 +0900

    feat: PS_MooveIt 구현

--- Assets/Scripts/1_Skill/Passive/PS_MooveIt.cs
+++ Assets/Scripts/1_Skill/Passive/PS_MooveIt.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PS_MooveIt : MonoBehaviour
+public class PS_MooveIt : Passive
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("쿨타임 감소 설정")]
+    [Tooltip("대쉬 쿨타임 감소 비율 (0.4 = 40% 감소)")]
+    [Range(0f, 1f)]
+    public float reductionRate = 0.4f;
+
+    protected override void Subscribe(AbilityEvents e)
     {
-        
+        e.OnModifyCooldown += OnModifyCooldown;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Unsubscribe(AbilityEvents e)
     {
-        
+        e.OnModifyCooldown -= OnModifyCooldown;
+    }
+
+    private void OnModifyCooldown(SkillType slot, ref float seconds)
+    {
+        if (slot != SkillType.Dash) return;
+        float factor = 1f - reductionRate;
+        seconds *= factor;
     }
 }
