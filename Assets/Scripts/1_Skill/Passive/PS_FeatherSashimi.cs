dc591b4bc376b4a5982a5979526bc87db04f5ab3
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-12-01 10:48:37 +0900

    feat: PS_FeatherSashimi 구현

--- Assets/Scripts/1_Skill/Passive/PS_FeatherSashimi.cs
+++ Assets/Scripts/1_Skill/Passive/PS_FeatherSashimi.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PS_FeatherSashimi : MonoBehaviour
+public class PS_FeatherSashimi : Passive
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Feather Sashimi 설정")]
+    [Tooltip("깃털 근접 공격 이펙트 프리팹 (예: Slash VFX)")]
+    public GameObject featherEffectPrefab;
+
+    [Tooltip("새로운 근접 공격 쿨타임")]
+    public float newCooldown = 1.0f;
+
+    private Skill meleeSkill;
+    private GameObject originalProjectilePrefab;
+
+    protected override void Subscribe(AbilityEvents e)
     {
-        
+        if (ability != null)
+        {
+            meleeSkill = ability.GetSkill(SkillType.Melee);
+        }
+
+        if (meleeSkill != null && featherEffectPrefab != null)
+        {
+            originalProjectilePrefab = meleeSkill.objSkillEntity;
+            meleeSkill.aimRange = 2;
+            meleeSkill.objSkillEntity = featherEffectPrefab;
+        }
+
+        e.OnModifyCooldown += OnModifyCooldown;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Unsubscribe(AbilityEvents e)
     {
-        
+        e.OnModifyCooldown -= OnModifyCooldown;
+        if (meleeSkill != null && originalProjectilePrefab != null)
+        {
+            meleeSkill.objSkillEntity = originalProjectilePrefab;
+        }
+    }
+    private void OnModifyCooldown(SkillType slot, ref float seconds)
+    {
+        if (slot != SkillType.Melee) return;
+        seconds = newCooldown;
     }
 }
