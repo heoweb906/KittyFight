3098580e8fa38cc84ae06015390639ad1c47de2f
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-12-01 10:49:41 +0900

    feat: PS_FeralBloodlust 구현

--- Assets/Scripts/1_Skill/Passive/PS_FeralBloodlust.cs
+++ Assets/Scripts/1_Skill/Passive/PS_FeralBloodlust.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PS_FeralBloodlust : MonoBehaviour
+public class PS_FeralBloodlust : Passive
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("피해량 증폭 설정")]
+    [Tooltip("적에게 주는 피해에 더해질 값")]
+    public int extraDealDamage = 20;
+
+    [Tooltip("내가 받는 피해에 더해질 값")]
+    public int extraTakenDamage = 20;
+
+    protected override void Subscribe(AbilityEvents e)
     {
-        
+        e.OnBeforeDealDamage += OnBeforeDealDamage;
+        e.OnBeforeTakeDamage += OnBeforeTakeDamage;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Unsubscribe(AbilityEvents e)
     {
-        
+        e.OnBeforeDealDamage -= OnBeforeDealDamage;
+        e.OnBeforeTakeDamage -= OnBeforeTakeDamage;
+    }
+
+    private void OnBeforeDealDamage(ref int dmg, GameObject victim)
+    {
+        if (dmg <= 0) return;
+        if (extraDealDamage == 0) return;
+        dmg += extraDealDamage;
+    }
+
+    private void OnBeforeTakeDamage(ref int dmg, GameObject attacker)
+    {
+        if (dmg <= 0) return;
+        if (extraTakenDamage == 0) return;
+        dmg += extraTakenDamage;
     }
 }
