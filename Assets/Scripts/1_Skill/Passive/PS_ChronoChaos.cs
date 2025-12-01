78c1d464fc8c0369e7f8655b7f893ab285fc40f4
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-12-01 10:50:46 +0900

    feat: PS_ChronoChaos 구현

--- Assets/Scripts/1_Skill/Passive/PS_ChronoChaos.cs
+++ Assets/Scripts/1_Skill/Passive/PS_ChronoChaos.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PS_ChronoChaos : MonoBehaviour
+public class PS_ChronoChaos : Passive
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("랜덤 쿨타임 보정 범위")]
+    [Tooltip("적용 가능한 쿨타임 보정값 목록 (0은 포함하지 말 것)")]
+    public int[] offsets = new int[] { -3, -2, -1, 1, 2, 3, 4, 5 };
+
+    protected override void Subscribe(AbilityEvents e)
     {
-        
+        e.OnModifyCooldown += OnModifyCooldown;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Unsubscribe(AbilityEvents e)
     {
-        
+        e.OnModifyCooldown -= OnModifyCooldown;
+    }
+
+    private void OnModifyCooldown(SkillType type, ref float duration)
+    {
+        if (duration <= 0f) return;
+        if (offsets == null || offsets.Length == 0) return;
+
+        int index = Random.Range(0, offsets.Length);
+        int delta = offsets[index];
+
+        if (delta == 0) return;
+
+        duration += delta;
+
+        if (duration < 0f) duration = 0f;
     }
 }
