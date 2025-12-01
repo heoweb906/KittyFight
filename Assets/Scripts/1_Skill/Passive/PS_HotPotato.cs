724d543af857712864da9cd07e343eeb69357957
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-12-01 10:44:18 +0900

    feat: PS_HotPotato 구현

--- Assets/Scripts/1_Skill/Passive/PS_HotPotato.cs
+++ Assets/Scripts/1_Skill/Passive/PS_HotPotato.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PS_HotPotato : MonoBehaviour
+public class PS_HotPotato : Passive
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("투사체 프리팹")]
+    public GameObject hotProjectilePrefab;
+
+    [Header("핫 포테이토 스펙")]
+    [Tooltip("원거리 쿨타임")]
+    public float hotCooldown = 5f;
+
+    private Skill rangedSkill;
+    private GameObject originalProjectilePrefab;
+
+    protected override void Subscribe(AbilityEvents e)
     {
-        
+        base.Subscribe(e);
+
+        if (ability != null)
+        {
+            rangedSkill = ability.GetSkill(SkillType.Ranged);
+        }
+
+        if (rangedSkill != null && hotProjectilePrefab != null)
+        {
+            originalProjectilePrefab = rangedSkill.objSkillEntity;
+            rangedSkill.objSkillEntity = hotProjectilePrefab;
+        }
+
+        e.OnModifyCooldown += OnModifyCooldown;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Unsubscribe(AbilityEvents e)
     {
-        
+        e.OnModifyCooldown -= OnModifyCooldown;
+
+        // 패시브 해제 시, 원래 원거리 투사체로 복원
+        if (rangedSkill != null && originalProjectilePrefab != null)
+        {
+            rangedSkill.objSkillEntity = originalProjectilePrefab;
+        }
+
+        base.Unsubscribe(e);
+    }
+
+    private void OnModifyCooldown(SkillType slot, ref float seconds)
+    {
+        if (slot != SkillType.Ranged) return;
+        seconds = hotCooldown;
     }
 }
