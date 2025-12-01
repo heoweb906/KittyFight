66a6d231274e755a838f0eb67cd99e8fb62bf15b
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-12-01 10:46:09 +0900

    feat: PS_WoollyGuard 구현

--- Assets/Scripts/1_Skill/Passive/PS_WoollyGuard.cs
+++ Assets/Scripts/1_Skill/Passive/PS_WoollyGuard.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PS_WoollyGuard : MonoBehaviour
+public class PS_WoollyGuard : Passive
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("받는 피해 감소 설정")]
+    [Tooltip("받는 피해 감소")]
+    public int damageDivisor = 12;
+
+    protected override void Subscribe(AbilityEvents e)
     {
-        
+        e.OnBeforeTakeDamage += OnBeforeTakeDamage;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Unsubscribe(AbilityEvents e)
     {
-        
+        e.OnBeforeTakeDamage -= OnBeforeTakeDamage;
+    }
+
+    private void OnBeforeTakeDamage(ref int dmg, GameObject attacker)
+    {
+        if (dmg <= 0) return;
+        if (damageDivisor <= 0f) return;
+
+        dmg -= damageDivisor;
     }
 }
