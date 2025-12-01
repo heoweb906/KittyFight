e02d9eb63aa84a474098f36401eb8ba1577ba30f
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-12-01 10:49:15 +0900

    feat: PS_Frenzy 구현

--- Assets/Scripts/1_Skill/Passive/PS_Frenzy.cs
+++ Assets/Scripts/1_Skill/Passive/PS_Frenzy.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PS_Frenzy : MonoBehaviour
+public class PS_Frenzy : Passive
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Subscribe(AbilityEvents e)
     {
-        
+        if (ability == null) return;
+
+        ability.OnSkillEquipped += OnSkillEquipped;
+        ForceConvertExistingSlots();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Unsubscribe(AbilityEvents e)
     {
-        
+        if (ability != null)
+        {
+            ability.OnSkillEquipped -= OnSkillEquipped;
+        }
+    }
+
+    private void ForceConvertExistingSlots()
+    {
+        if (ability == null) return;
+
+        ConvertSlotToMelee(SkillType.Ranged);
+        ConvertSlotToMelee(SkillType.Skill1);
+        ConvertSlotToMelee(SkillType.Skill2);
+    }
+
+    private void ConvertSlotToMelee(SkillType slot)
+    {
+        if (ability == null) return;
+
+        var s = ability.GetSkill(slot);
+        if (s == null) return;
+
+        // SetSkill을 한 번 호출해서 OnSkillEquipped 이벤트를 발생시킴
+        // 아래 OnSkillEquipped 핸들러에서 실제로 Melee로 갈아끼운다.
+        ability.SetSkill(slot, s);
+    }
+
+    private void OnSkillEquipped(SkillType type, Skill skill)
+    {
+        if (ability == null) return;
+
+        if (type == SkillType.Melee)
+        {
+            ForceConvertExistingSlots();
+            return;
+        }
+
+        if (type == SkillType.Dash) return;
+
+        var melee = ability.GetSkill(SkillType.Melee);
+        if (melee == null) return;
+
+        if (skill == melee) return;
+
+        ability.SetSkill(type, melee);
     }
 }
