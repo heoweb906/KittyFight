9f197480904a6893a5c2283ff853562bd1dbfb1f
minjae4650 <121929064+minjae4650@users.noreply.github.com>
2025-12-29 16:03:34 +0900

    fix: 맞도록 수정

--- Assets/Scripts/1_Skill/Skills/SK_SavageRoar.cs
+++ Assets/Scripts/1_Skill/Skills/SK_SavageRoar.cs
@@ -21,17 +21,19 @@ public class SK_SavageRoar : Skill
         var rot = Quaternion.LookRotation(direction, Vector3.up);
         var go = Instantiate(objSkillEntity, origin, rot);
 
-        var ab = go.GetComponent<AB_HitboxBase>();
-        if (ab != null) ab.Init(playerAbility);
-
-        else if (playerAbility.playerNumber == MatchResultStore.myPlayerNumber)
+        var hitboxes = go.GetComponentsInChildren<AB_HitboxBase>(true);
+        for (int i = 0; i < hitboxes.Length; i++)
         {
-            var gm = FindObjectOfType<GameManager>();
+            hitboxes[i].Init(playerAbility);
+        }
+
+        var gm = FindObjectOfType<GameManager>();
+        if (playerAbility.playerNumber == MatchResultStore.myPlayerNumber)
+        {
             gm?.cameraManager?.ShakeCameraPunch(shakeAmount, shakeAmount, direction);
         }
         else
         {
-            var gm = FindObjectOfType<GameManager>();
             gm?.cameraManager?.ShakeCameraPunch(shakeAmount * 0.5f, shakeAmount * 0.5f, direction);
         }
     }
